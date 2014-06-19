ca2c308a1fcf98eab0ad358514cfdfd918a5a8f6
Sean McArdle <sean.mcardle0@gmail.com>
2014-06-18 18:03:23 -0700

    Disabled whitespace indicator

--- personal/custom-init.el
+++ personal/custom-init.el
@@ -11,7 +11,7 @@
 (package-initialize)
 
 (defvar personal-packages
-             '(fill-column-indicator mark-multiple rect-mark flymake))
+             '(fill-column-indicator mark-multiple rect-mark flymake smex))
 
 ;; This block was taken from the a Prelude init script and modified
 
@@ -40,9 +40,13 @@
 (setq fci-rule-width 2)
 (setq fci-rule-color "black")
 
+
+;; disable whitespace indicator
+(setq prelude-whitespace nil)
+
 ;; Set global font
 (when (eq system-type 'gnu/linux)
-    (set-frame-font "Ubuntu Mono-10" nil))
+    (set-frame-font "Monaco-10" nil))
 (when (eq system-type 'windows-nt)
     (set-frame-font "Consolas-10" nil))
 (when (eq system-type 'darwin)
@@ -77,3 +81,54 @@
 
 (provide 'custom-init)
 ;;; custom-init.el ends here
+
+
+;; Open file as root?
+(defun th-rename-tramp-buffer ()
+  (when (file-remote-p (buffer-file-name))
+    (rename-buffer
+     (format "%s:%s"
+             (file-remote-p (buffer-file-name) 'method)
+             (buffer-name)))))
+
+(add-hook 'find-file-hook
+          'th-rename-tramp-buffer)
+
+(defadvice find-file (around th-find-file activate)
+  "Open FILENAME using tramp's sudo method if it's read-only."
+  (if (and (not (file-writable-p (ad-get-arg 0)))
+           (y-or-n-p (concat "File "
+                             (ad-get-arg 0)
+                             " is read-only.  Open it as root? ")))
+      (th-find-file-sudo (ad-get-arg 0))
+    ad-do-it))
+
+(defun th-find-file-sudo (file)
+  "Opens FILE with root privileges."
+  (interactive "F")
+  (set-buffer (find-file (concat "/sudo::" file))))
+
+
+;; Backup files
+(setq user-emacs-directory "~/.emacs.d")
+
+(setq backup-directory-alist
+      (list (cons "." (expand-file-name "backup" user-emacs-directory))))
+
+
+;;ido mode
+(require 'ido)
+(ido-mode t)
+
+;; smex
+(require 'smex)
+(smex-initialize)
+
+;; column number mode
+(column-number-mode)
+(setq default-tab-width 4)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+(global-set-key (kbd "\M- ") 'hippie-expand)
