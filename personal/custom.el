6956300becdeed5ffb6399e1b094ffba6d01a775
Sean McArdle <sean.mcardle0@gmail.com>
2015-04-11 21:58:38 -0700

    Fixef merge conflict

--- personal/custom.el
+++ personal/custom.el
@@ -142,16 +142,13 @@
 
 ;; jedi python completion
 (autoload 'jedi:setup "jedi" nil t)
-(add-hook 'python-mode-hook 'jedi:ac-setup)
+(add-hook 'python-mode-hook 'jedi:setup)
 
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
-
-
-;; Disable quote completion
-(require 'smartparens)
-(setq sp-autoescape-string-quote nil)
-
+(setq jedi:environment-root "jedi")  ; or any other name you like
+(setq jedi:environment-virtualenv
+      (list "pyvenv" "--system-site-packages"))
 
 ;; Auto-wrap at 80 char in Org Mode files
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
@@ -171,3 +168,11 @@
 
 ;; Just for fun
 (global-set-key (kbd "C-c C-c z") 'zone)
+
+;; Disable smartparens and enable autopair
+(turn-off-smartparens-mode)
+(require 'autopair)
+(autopair-global-mode)
+
+(provide 'custom)
+;;; custom.el ends here
