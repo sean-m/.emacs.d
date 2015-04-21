f0b205c9a46ae8de2eb63b834f0891baac55ab35
Sean McArdle <sean.mcardle0@gmail.com>
2015-04-21 12:23:20 -0700

    Using prelude for additional package installs.

--- personal/custom.el
+++ personal/custom.el
@@ -8,25 +8,9 @@
 (load-library "~/.emacs.d/personal/repo.el")
 
 
-; list the packages you want
-(setq package-list '(autopair powershell csharp-mode jedi fill-column-indicator mark-multiple rect-mark flymake smex))
+; install aditional packages
+(prelude-require-packages '(autopair powershell csharp-mode jedi fill-column-indicator mark-multiple rect-mark flymake smex))
 
-; list the repositories containing them
-(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
-
-; activate all the packages (in particular autoloads)
-(package-initialize)
-
-; fetch the list of packages available
-(unless package-archive-contents
-  (package-refresh-contents))
-
-; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
 
 ;; Show line numbers in left margin
 (global-linum-mode 1)
