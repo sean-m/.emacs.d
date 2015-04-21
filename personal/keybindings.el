db578127627f36b7f94e8b3ca01f796eabd36077
Sean McArdle <sean.mcardle0@gmail.com>
2015-04-21 16:57:47 -0700

    Updated to use phi-rectangle

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -15,19 +15,6 @@
 ;; Make rectangle selections visible
 (require 'mark-multiple)
 (require 'phi-rectangle)
-(global-set-key (kbd "C-x r C-SPC") 'rm-set-mark)
-(global-set-key (kbd "C-w")
-                '(lambda(b e) (interactive "r")
-                   (if rm-mark-active
-                       (rm-kill-region b e) (kill-region b e))))
-(global-set-key (kbd "M-w")
-                '(lambda(b e) (interactive "r")
-                   (if rm-mark-active
-                       (rm-kill-ring-save b e) (kill-ring-save b e))))
-(global-set-key (kbd "C-x C-x")
-                '(lambda(&optional p) (interactive "p")
-                   (if rm-mark-active
-                       (rm-exchange-point-and-mark p) (exchange-point-and-mark p))))
 
 ;; Line Join
 (global-set-key (kbd "M-j")
